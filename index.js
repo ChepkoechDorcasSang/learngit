4228669c2127d2f8af17f28420f47227d83d8f96
ChepkoechDorcasSang <dorcas41975173@gmail.com>
2024-03-11 12:53:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*let myName="Dorcas"
+myName="Chepkoech"
+ myName=5
+ 
+console.log(myName)*/
+
+let myNumber =6
+ if (myNumber===56){
+    console.log("true")
+ }
+ else{
+    console.log("false")
+ }
