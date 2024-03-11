d4e6f87c4b820778d1df69cab6e2aeafc4d1b50b
ChepkoechDorcasSang <dorcas41975173@gmail.com>
2024-03-11 13:10:10 +0300

    first comment

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ myName="Chepkoech"
  
 console.log(myName)*/
 
-let myNumber =6
+/*let myNumber =6
  if (myNumber===56){
     console.log("true")
  }
  else{
     console.log("false")
- }
+ }*/
+ let myNumber=5
+ let myValue=7
+ let sum=myNumber+myValue
+ let product=myNumber*myValue
+ let division=myNumber/myValue
+ let difference=myNumber-myValue
+ console.log("\nsum:",sum);
+ console.log("\nproduct:",product);
+ console.log("\ndivision:",division);
+ console.log("\ndifference:",difference);
